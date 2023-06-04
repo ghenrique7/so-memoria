1172558090bda45464c95e4ea6eee336b6acdf9e
Gabriel Henrique <gcoelho730@gmail,com>
2023-06-04 14:05:50 -0300

    quemerdavey

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    worker: {
-      plugins: [react()],
-    },
     build: {
-      // generate manifest.json in outDir
-      manifest: true,
-      base: '/public/build/',
-    }
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          nested: resolve(__dirname, 'chat/index.html'),
+        },
+      },
+    },
 })
