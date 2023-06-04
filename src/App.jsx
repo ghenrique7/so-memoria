170b60813096ceddfc2eebc573427daec6367961
Gabriel Henrique <gcoelho730@gmail,com>
2023-06-04 13:41:51 -0300

    maldiçao

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-
-const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
